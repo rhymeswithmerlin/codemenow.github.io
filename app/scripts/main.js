4901fa2f7be682b1751ad65b20294985fd9a8cd6
CodeMeNow <rhymeswithmerlin@gmail.com>
2017-07-07 18:03:03 -0400

    first portfolio commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,1 @@
-console.log('Greetings! I am currently looking for new opportunities');
+console.log('Greetings! I am currently looking for new opportunities\n     -----     \n     I   I    \n---------------\nI-------------I'\n---------------\nI-------------I');
